a2f22d5255d6fd6529d248260428c3dbdeb7ba77
arturbl <arturesmavc@gmail.com>
2022-04-11 23:04:01 +0100

    Final project GUI without map done

--- finalProject/src/Map.java
+++ finalProject/src/Map.java
@@ -1,11 +1,52 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.util.Hashtable;
 
 public class Map {
+
     private JPanel mainPanel;
-    private JSlider xSlider;
     private JPanel headerPanel;
     private JPanel bodyPanel;
     private JPanel mapPanel;
     private JPanel ySliderPanel;
     private JLabel infoLabel;
+    private JSlider xSlider;
+    private JSlider ySlider;
+
+    public Map() {
+        addSlidersDetails(0,300, xSlider);
+        addSlidersDetails(0,600, ySlider);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Map");
+        frame.setContentPane(new Map().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void addSlidersDetails(int min, int max, JSlider slider) {
+
+        Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
+        labelTable.put( min, new JLabel( Integer.toString(min) ) );
+        labelTable.put( max, new JLabel(Integer.toString(max) ) );
+        slider.setLabelTable( labelTable );
+        slider.setPaintLabels(true);
+
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                updateLabel();
+            }
+
+        });
+    }
+
+    private void updateLabel() {
+        String text = "x: " + xSlider.getValue() + " y:" + ySlider.getValue();
+        infoLabel.setText(text);
+    }
+
 }
